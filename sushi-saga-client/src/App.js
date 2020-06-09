3a69583a0bf13c59d2f04390d4b5d09d818396d2
github username <github email address>
2020-06-09 11:18:11 -0400

    core deliverables met

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
   state = {
 	sushis: [],
 	startIndex: 0,
-	  tableItems: []
+	tableItems: [],
+	tableMoney: 100
   }
 
   componentDidMount(){
@@ -30,15 +31,24 @@ class App extends Component {
   }
 
   handleEatSushi = e => {
-	  const sushiEaten = this.state.sushis.filter(sushi => sushi.id == e.target.id)
-	  console.log(sushiEaten)
+  	  const sushiId = Number(e.target.id || e.target.parentNode.id)
+	  const sushiEaten = this.state.sushis.filter(sushi => sushi.id === sushiId )[0]
+  	  const sushiIndex = this.state.sushis.indexOf(sushiEaten)
+  	  if ((this.state.tableMoney - sushiEaten.price) >= 0){
+  	  	  this.setState(prevState => ({
+		  	  sushis: prevState.sushis.filter(sushi => sushi.id !== sushiId),
+		  	  tableItems: [sushiEaten, ...prevState.tableItems],
+		  	  tableMoney: prevState.tableMoney - sushiEaten.price
+  	  	  }))
+  	  	 }
   }
 
   render() {
+  	  console.log(this.state)
     return (
       <div className="app">
         <SushiContainer sushis={this.returnFourSushis()} showMore={this.handleShowMore} handleEat={this.handleEatSushi} />
-        <Table />
+        <Table tableItems={this.state.tableItems} money={this.state.tableMoney} />
       </div>
     );
   }
